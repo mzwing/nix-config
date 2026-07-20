c1b7ab0ecd24622891de0e410456f5e526489f4c
mzwing <mzwing@mzwing.eu.org>
2026-07-20 17:53:54 +0800

    feat: try to use pi instead of codex & opencode

--- modules/features/software/vibecoding.nix
+++ modules/features/software/vibecoding.nix
@@ -33,17 +33,44 @@
           enable = true;
           enableMcpIntegration = true;
         };
-        codex = {
-          enable = true;
-          enableMcpIntegration = true;
-        };
-        opencode = {
+        pi-coding-agent = {
           enable = true;
           extraPackages = with pkgs; [
-            rtk
+            git
+            nodejs
+            pnpm
           ];
-          web.enable = true;
-          enableMcpIntegration = true;
+          settings = {
+            retry = {
+              enabled = true;
+              maxRetries = 3;
+            };
+            theme = "dark";
+            npmCommand = ["pnpm"];
+            packages = [
+              "git:github.com/justhil/pi-ace-tool"
+              "git:github.com/justhil/pi-fast-context"
+              "npm:@complexthings/pi-dynamic-context-pruning"
+              "npm:@ff-labs/pi-fff"
+              "npm:@gotgenes/pi-permission-system"
+              "npm:@gotgenes/pi-subagents"
+              "npm:@gotgenes/pi-subagents-worktrees"
+              "npm:@juicesharp/rpiv-ask-user-question"
+              "npm:@narumitw/pi-btw"
+              "npm:@narumitw/pi-plan-mode"
+              "npm:@upstash/context7-pi"
+              "npm:pi-codex-goal"
+              "npm:pi-markdown-preview"
+              "npm:pi-mcp-adapter"
+              "npm:pi-nano-context"
+              "npm:pi-simplify"
+              "npm:pi-smart-fetch"
+              "npm:pi-tool-display"
+              "npm:pi-web-access"
+              "npm:pi-workspace-history"
+              "npm:pi-wtf"
+            ];
+          };
         };
       };
     };
