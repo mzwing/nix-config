49509b410114bd71ca43d84877c127b8f790f24d
mzwing <mzwing@mzwing.eu.org>
2026-08-02 16:46:22 +0000

    feat(software): use system module for pumpkin

--- modules/features/software/pumpkin.nix
+++ modules/features/software/pumpkin.nix
@@ -1,5 +1,39 @@
 let
-  javaPort = 3389;
+  javaPort = 8880;
+
+  pumpkinService = {
+    enable = true;
+
+    settings = {
+      allow_chat_reports = false;
+
+      logging.color = true;
+
+      world.autosave_ticks = 6000;
+
+      commands = {
+        use_console = false;
+        use_tty = false;
+      };
+
+      networking = {
+        query.enabled = false;
+        rcon.enabled = false;
+        proxy.enabled = false;
+        lan_broadcast.enabled = false;
+
+        java = {
+          enabled = true;
+          address = "0.0.0.0:${toString javaPort}";
+          encryption = true;
+          online_mode = false;
+          authentication.enabled = false;
+        };
+
+        bedrock.enabled = false;
+      };
+    };
+  };
 in {
   mzwing.features."software/pumpkin" = {
     meta.platforms = [
@@ -7,46 +41,22 @@ in {
       "nixos"
     ];
 
-    nixos.networking.firewall.allowedTCPPorts = [javaPort];
-
-    home = {inputs, ...}: {
+    darwin = {inputs, ...}: {
       imports = [
-        inputs.nur.repos.mzwing.modules.homeManager.pumpkin
+        inputs.nur.repos.mzwing.modules.darwin.pumpkin
       ];
 
-      services.pumpkin = {
-        enable = true;
+      services.pumpkin = pumpkinService;
+    };
 
-        settings = {
-          allow_chat_reports = false;
+    nixos = {inputs, ...}: {
+      imports = [
+        inputs.nur.repos.mzwing.modules.nixos.pumpkin
+      ];
 
-          logging.color = false;
+      networking.firewall.allowedTCPPorts = [javaPort];
 
-          world.autosave_ticks = 6000;
-
-          commands = {
-            use_console = false;
-            use_tty = false;
-          };
-
-          networking = {
-            query.enabled = false;
-            rcon.enabled = false;
-            proxy.enabled = false;
-            lan_broadcast.enabled = false;
-
-            java = {
-              enabled = true;
-              address = "0.0.0.0:${toString javaPort}";
-              encryption = true;
-              online_mode = false;
-              authentication.enabled = false;
-            };
-
-            bedrock.enabled = false;
-          };
-        };
-      };
+      services.pumpkin = pumpkinService;
     };
   };
 }
