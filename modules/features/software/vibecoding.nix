f3f4591266e1f78d2412ee48445144ebd178b062
mzwing <mzwing@mzwing.eu.org>
2026-08-21 14:09:50 +0000

    fix(software): remove the fxxking tau-mirror pi extension

--- modules/features/software/vibecoding.nix
+++ modules/features/software/vibecoding.nix
@@ -387,7 +387,6 @@ in {
               "npm:@narumitw/pi-plan-mode"
               "npm:@narumitw/pi-usage"
               "npm:@upstash/context7-pi"
-              "npm:tau-mirror"
               "npm:pi-codex-goal"
               "npm:pi-effort"
               "npm:pi-markdown-preview"
