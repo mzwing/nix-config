1f320783a2f71006d318ee926a886dda20755a9a
mzwing <mzwing@mzwing.eu.org>
2026-09-06 10:42:32 +0000

    feat(software): use nixpkgs' pumpkin module

--- modules/features/software/pumpkin.nix
+++ modules/features/software/pumpkin.nix
@@ -1,62 +1,44 @@
-let
-  javaPort = 8880;
-
-  pumpkinService = {
-    enable = true;
-
-    settings = {
-      allow_chat_reports = false;
-
-      logging.color = true;
-
-      world.autosave_ticks = 6000;
-
-      commands = {
-        use_console = false;
-        use_tty = false;
-      };
-
-      networking = {
-        query.enabled = false;
-        rcon.enabled = false;
-        proxy.enabled = false;
-        lan_broadcast.enabled = false;
-
-        java = {
-          enabled = true;
-          address = "0.0.0.0:${toString javaPort}";
-          encryption = true;
-          online_mode = false;
-          authentication.enabled = false;
-        };
-
-        bedrock.enabled = false;
-      };
-    };
-  };
-in {
+{
   mzwing.features."software/pumpkin" = {
-    meta.platforms = [
-      "darwin"
-      "nixos"
-    ];
+    meta.platforms = ["nixos"];
 
-    darwin = {inputs, ...}: {
-      imports = [
-        inputs.nur.repos.mzwing.modules.darwin.pumpkin
-      ];
+    nixos = {
+      services.pumpkin = {
+        enable = true;
 
-      services.pumpkin = pumpkinService;
-    };
+        # Reads the port back out of settings.networking.java.address.
+        openFirewall = true;
 
-    nixos = {inputs, ...}: {
-      imports = [
-        inputs.nur.repos.mzwing.modules.nixos.pumpkin
-      ];
+        settings = {
+          allow_chat_reports = false;
 
-      networking.firewall.allowedTCPPorts = [javaPort];
+          logging.color = true;
 
-      services.pumpkin = pumpkinService;
+          world.autosave_ticks = 6000;
+
+          commands = {
+            use_console = false;
+            use_tty = false;
+          };
+
+          networking = {
+            query.enabled = false;
+            rcon.enabled = false;
+            proxy.enabled = false;
+            lan_broadcast.enabled = false;
+
+            java = {
+              enabled = true;
+              address = "0.0.0.0:8880";
+              encryption = true;
+              online_mode = false;
+              authentication.enabled = false;
+            };
+
+            bedrock.enabled = false;
+          };
+        };
+      };
     };
   };
 }
