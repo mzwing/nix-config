57b08fe09cec5c27084828ef086e7c4ef6549232
mzwing <mzwing@mzwing.eu.org>
2026-09-14 09:26:25 +0800

    chore: update devenv writing

--- devenv.nix
+++ devenv.nix
@@ -3,27 +3,30 @@
   pkgs,
   ...
 }: {
-  overlays = [inputs.nur.overlays.default];
-
   languages.nix = {
     enable = true;
     lsp.enable = true;
   };
 
-  packages = with pkgs; [
-    act
-    actionlint
-    alejandra
-    just
-    nixd
-    nixos-rebuild-ng
-    ruff
-    shellcheck
-    shfmt
-    ty
-    inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
-    nur.repos.mzwing.typenix
-  ];
+  packages =
+    (with pkgs; [
+      act
+      actionlint
+      alejandra
+      just
+      nixd
+      nixos-rebuild-ng
+      ruff
+      shellcheck
+      shfmt
+      ty
+      inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ])
+    ++ (with inputs.nur-packages.packages.${pkgs.stdenv.hostPlatform.system}; [
+      typenix
+    ]);
+
+  cachix.pull = ["mzwing"];
 
   enterTest = ''
     act --version
