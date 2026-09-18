2908956e199c0dfb5c4c346e9a66a7a7aed3dc1e
mzwing <mzwing@mzwing.eu.org>
2026-09-18 16:40:33 +0000

    fix(environments): fix rust devenv config

--- environments/rust/devenv.nix
+++ environments/rust/devenv.nix
@@ -4,8 +4,9 @@
     channel = "nightly";
     lsp.enable = true;
     cranelift.enable = true;
-    mold.enable = true; # On MacOS, use this
-    # wild.enable = true; # On NixOS, use this
+    # on NixOS, uncomment the following line
+    # cranelift.enable = true;
+    # wild.enable = true;
   };
 
   git-hooks.hooks = {
